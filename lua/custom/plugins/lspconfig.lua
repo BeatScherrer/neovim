9408a61c4b9e0a2b6c8a9aafab2f0eb1c8475996
Beat Scherrer <beat@P1.localdomain>
2024-06-17 17:16:53 +0200

    add angular ls

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -159,6 +159,7 @@ return {
         nixd = {},
         taplo = {},
         tsserver = {},
+        angularls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
