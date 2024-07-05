4eb218466d588940dc5deb041fc7ca7ab7b7f2dd
Beat Scherrer <beat.scherrer@gmail.com>
2024-07-05 09:29:25 +0200

    add docker ls

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -178,6 +178,7 @@ return {
           --   "--stdio",
           -- },
         },
+        dockerls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
