36d21913e9bd7eaa47beffc433ed007151275a11
BeatScherrer <beat.scherrer@gmail.com>
2024-07-24 22:14:16 +0200

    migrate which-key

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -21,21 +21,31 @@ return {
       require("which-key").setup()
 
       -- Document existing key chains
-      require("which-key").register({
-        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-        ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-        ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-        ["<leader>u"] = { name = "[U]ser Interface", _ = "which_key_ignore" },
-        ["<leader>un"] = { name = "[N]oice", _ = "which_key_ignore" },
-        ["<leader><Tab>"] = { name = "[T]ab", _ = "which_key_ignore" },
+      require("which-key").add({
+        { "<leader><Tab>", group = "[T]ab" },
+        { "<leader><Tab>_", hidden = true },
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>c_", hidden = true },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>d_", hidden = true },
+        { "<leader>h", group = "Git [H]unk" },
+        { "<leader>h_", hidden = true },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>r_", hidden = true },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>s_", hidden = true },
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>t_", hidden = true },
+        { "<leader>u", group = "[U]ser Interface" },
+        { "<leader>u_", hidden = true },
+        { "<leader>un", group = "[N]oice" },
+        { "<leader>un_", hidden = true },
+        { "<leader>w", group = "[W]orkspace" },
+        { "<leader>w_", hidden = true },
       })
       -- visual mode
-      require("which-key").register({
-        ["<leader>h"] = { "Git [H]unk" },
+      require("which-key").add({
+        { "<leader>h", desc = "Git [H]unk", mode = "v" },
       }, { mode = "v" })
     end,
   },
