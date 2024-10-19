fa0947e8c9bd5ccfafec099b5be8e86a9dd0c653
BeatScherrer <beat.scherrer@gmail.com>
2024-10-19 13:32:02 +0200

    update neo-tree

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -182,6 +182,7 @@ return {
         lemminx = {},
         bashls = {},
         html = {},
+        emmet_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
