17b02ef5159321465b4bd036c3d186f838010452
Beat Scherrer <beat.scherrer@gmail.com>
2025-03-10 16:55:41 +0100

    add tailwindcss lsp config

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -144,6 +144,7 @@ return {
         bashls = {},
         html = {},
         emmet_ls = {},
+        tailwindcss = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
