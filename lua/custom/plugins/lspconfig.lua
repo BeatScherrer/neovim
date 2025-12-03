6444cacd5c17683fe222e5322b23fe8ecc404122
Beat Scherrer <beat.scherrer@gmail.com>
2025-12-03 09:23:50 +0100

    add csharp_ls

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -159,11 +159,11 @@ return {
           settings = {
             python = {
               analysis = {
-                autoImportCompletions = true;
-                autoSearchPaths = true
-              }
-            }
-          }
+                autoImportCompletions = true,
+                autoSearchPaths = true,
+              },
+            },
+          },
         },
         pylsp = {},
         dockerls = {},
@@ -172,6 +172,7 @@ return {
         html = {},
         emmet_ls = {},
         tailwindcss = {},
+        csharp_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
