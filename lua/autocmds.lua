1a02e944541b848a48c5b41ad682cb08eb390aa2
Beat Scherrer <beat.scherrer@gmail.com>
2026-01-09 10:36:54 +0100

    replace "" with <> in mtr includes

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,3 +5,14 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufFilePost" }, {
     vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
   end,
 })
+
+-- Replace #include "mt_xxx/yyy" with #include <xxx/yyy> on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.cpp", "*.h", "*.hpp" },
+  callback = function()
+    local file = vim.fn.expand("%:p")
+    vim.fn.system(string.format([[sed -i 's/#include "\(mt_.*\/.*\)"/#include <\1>/g' %s]], vim.fn.shellescape(file)))
+    -- Reload the buffer if the file was modified
+    vim.cmd("checktime")
+  end,
+})
